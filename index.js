049fe80d141b182fd61b310686334cd861467279
Chl0l <chloe.kim.elle@gmail.com>
2023-03-16 11:29:45 +0100

    Creation of Wilders folder with chalk module

---
+++ index.js
@@ -0,0 +1,7 @@
+import chalk from 'chalk';
+
+console.log(chalk.blue('In the Slytherin team we have:'));
+console.log(chalk.green('Denis'));
+console.log(chalk.red('Sébastien'));
+console.log(chalk.yellow('Alexis'));
+console.log(chalk.grey('Youcef'));
